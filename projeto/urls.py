7692758f27f364be825bce8999e9efd869fc4e46
Kaique Silva Sousa <ksilvasousa1@gmail.com>
2025-06-20 19:49:38 -0300

    Adicionando urls e vendo http

--- projeto/urls.py
+++ projeto/urls.py
@@ -16,7 +16,14 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.http import HttpResponse
+
+
+def my_py(request):
+    return HttpResponse("Hello, World!")
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path("outra-coisa/", my_py),
 ]
