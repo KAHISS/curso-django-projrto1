5c49c217e1a406d01523a39d89ddd8a6b7b70960
Kaique Silva Sousa <ksilvasousa1@gmail.com>
2025-06-23 15:31:51 -0300

    Arrumando links

--- recipes/views.py
+++ recipes/views.py
@@ -1,15 +1,16 @@
 from django.shortcuts import render
 
+from utils.recipes.main import make_recipe
+
 
 def home(request):
     return render(request, "recipes/pages/home.html", context={
-        "name": "João",
-        "surname": "Silva",
-    }, status=201)
+        "recipes": [make_recipe() for _ in range(10)],
+    })
 
 
 def recipe(request, id):
-    return render(request, "recipes/pages/home.html", context={
-        "name": "João",
-        "surname": "Silva",
-    }, status=201)
+    return render(request, "recipes/pages/recipe-view.html", context={
+        "recipe": make_recipe(),
+        "is_detail_page": True,
+    })
